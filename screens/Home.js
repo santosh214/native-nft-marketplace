c34eadb5c67cc88749a46bf138f48b5323397de6
santosh214 <santoshsingh74827@gmail.com>
2022-08-02 16:49:12 +0530

    project setup

---
+++ screens/Home.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View, FlatList, SafeAreaView } from 'react-native'
+import React, { useState } from 'react'
+// import {COLORS,NFTData} from '../constants'
+const Home = () => {
+    return (
+        <SafeAreaView>
+            <Text>Home</Text>
+        </SafeAreaView>
+    )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
